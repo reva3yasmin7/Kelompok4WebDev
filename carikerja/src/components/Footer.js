3d9b2aae343374e7f3d772439fafe8fc31595a6d
adzizPe <adzanaziz2@gmail.com>
2024-08-05 22:03:04 +0700

    react js

--- carikerja/src/components/Footer.js
+++ carikerja/src/components/Footer.js
@@ -9,15 +9,13 @@ function Footer() {
       <Container>
         <Row>
           <Col md={6}>
+            <div className="d-flex align-items-center mb-3">
               <img src={logoImage} alt="carikerja" className="logo" />
-              carikerja
-              <div className='mt-3'>
-              <p>Platform hebat untuk pencari kerja atau 
-                bakat yang <br></br>
-               memiliki minat terhadap startup. Temukan pekerjaan <br></br>impian Anda dengan lebih mudah.</p>
-              </div>
+              <h5 className="ms-2 mb-0">carikerja</h5>
+            </div>
+            <p>Platform hebat untuk pencari kerja atau bakat yang memiliki minat terhadap startup. Temukan pekerjaan impian Anda dengan lebih mudah.</p>
           </Col>
-          <Col md={3}>
+          <Col md={2}>
             <h5>About</h5>
             <ul className="list-unstyled">
               <li><a href="#developer" className="text-white">Developer</a></li>
@@ -25,14 +23,32 @@ function Footer() {
               <li><a href="#privacy" className="text-white">Privacy Policy</a></li>
             </ul>
           </Col>
-          <Col md={3}>
+          <Col md={2}>
             <h5>Contact</h5>
+            <ul className="list-unstyled">
+              <li><a href="#contact" className="text-white">Contact Us</a></li>
+            </ul>
+          </Col>
+          <Col md={2}>
+            <h5>Dapatkan informasi pekerjaan</h5>
+            <p>Berita pekerjaan terbaru, artikel, dikirim ke kotak masuk Anda setiap minggu.</p>
             <Form>
-              <Form.Group controlId="formBasicEmail">
-                <Form.Control type="email" placeholder="Enter email" />
+              <Form.Group controlId="formBasicEmail" className="d-flex">
+                <Form.Control type="email" placeholder="Alamat Email" className="me-2" />
+                <Button variant="secondary" type="submit" className='btn-primaryfooter'>Kirim</Button>
               </Form.Group>
-              <Button variant="secondary" type="submit" className="mt-2">Kirim</Button>
             </Form>
+          </Col>
+        </Row>
+        <Row className="mt-4">
+          <Col>
+            <div className="social-icons">
+              <a href="#"><i className="fab fa-facebook-f"></i></a>
+              <a href="#"><i className="fab fa-instagram"></i></a>
+              <a href="#"><i className="fab fa-google"></i></a>
+              <a href="#"><i className="fab fa-twitter"></i></a>
+              <a href="#"><i className="fab fa-linkedin-in"></i></a>
+            </div>
           </Col>
         </Row>
         <Row className="mt-4">
